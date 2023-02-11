5182fa498f91a8b1a00b30ad8b03c4ace39872f2
OmerGit <rafaelxdale@gmail.com>
2023-02-10 21:20:58 -0500

    new project

--- src/main/java/org/example/p01/p01.java
+++ src/main/java/org/example/p01/p01.java
@@ -1,5 +1,29 @@
 package org.example.p01;
 
 public class p01 {
+    private int[] persons;
 
+    public p01() {
+        this.persons = new int[]{1, 2, 3, 4, 5, 6};
+    }
+
+    private void oneMessage() {
+        System.out.print("[----] [----] [----] [----] [----] [----] \t\t\t");
+        for (int i = 0; i < persons.length; i++) {
+            if (persons[i] > 3) {
+                System.out.print("[" + "misionero" + "]");
+            } else {
+                System.out.print("[" + "canibal" + "]");
+            }
+        }
+    }
+
+    private void ruta() {
+
+    }
+
+    public static void main(String[] args) {
+        p01 p = new p01();
+        p.oneMessage();
+    }
 }
